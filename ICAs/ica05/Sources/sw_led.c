686bacab7feb22d180032eb48724b16b97cbd507
Bryce-Frey <111655958+Bryce-Frey@users.noreply.github.com>
2024-02-09 15:54:51 -0700

    ica05

---
+++ ICAs/ica05/Sources/sw_led.c
@@ -0,0 +1,24 @@
+#include <hidef.h>  
+#include "derivative.h"
+#include "sw_led.h"
+
+void SWL_Init(void){
+    SWL_LEDColour SWL_ALL;
+    DDR1AD1 = 0b11100000;
+    ATD1DIEN1 = 0b00011111;
+}
+int SWL_Pushed(SWL_SwitchPos pos){
+    int value = 0;
+    if((PT1AD1 & pos) == pos){
+        value = 1;
+    }
+    return value;
+}
+void SWL_ON (SWL_LEDColour led){
+    PT1AD1 |= led;
+}
+void SWL_TOG (SWL_LEDColour led){
+    if (((PT1AD1 | led) & SWL_ALL) != SWL_ALL){
+        PT1AD1 ^= led;
+    }    
+}
